abac2edce3832948de71bc392a7608f9e5296f2c
tiger <woactd@gmail.com>
2017-06-17 02:02:29 +0800

    Initialize the project!!!

---
+++ spring-servlet/src/main/java/com/dongly/SpringServletApplication.java
@@ -0,0 +1,12 @@
+package com.dongly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServletApplication.class, args);
+	}
+}
