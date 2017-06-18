c57c4ac1822e89f84a5c67571b4e6c5ec99b19d7
tiger <woactd@gmail.com>
2017-06-19 01:31:13 +0800

    整合mybatis

---
+++ spring-mybatis/src/main/java/com/dongly/SpringMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.dongly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybatisApplication.class, args);
+	}
+}
